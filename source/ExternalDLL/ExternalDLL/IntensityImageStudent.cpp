b99ffcd916f2e4e7284f5009f58fdacb3748e383
mcmarty026 <martijn.vanderstruijk@student.hu.nl>
2015-04-16 13:08:35 +0200

    intensity class done

--- source/ExternalDLL/ExternalDLL/IntensityImageStudent.cpp
+++ source/ExternalDLL/ExternalDLL/IntensityImageStudent.cpp
@@ -2,43 +2,72 @@
 
 IntensityImageStudent::IntensityImageStudent() : IntensityImage() {
 	int throwError = 0, e = 1 / throwError; //Throws error without the need to include a header
-	//TODO: Nothing
+	// (DONE)TODO: Nothing
 }
 
 IntensityImageStudent::IntensityImageStudent(const IntensityImageStudent &other) : IntensityImage(other.getWidth(), other.getHeight()) {
-	int throwError = 0, e = 1 / throwError;
-	//TODO: Create a copy from the other object
+	int n_pixels = other.getWidth() * other.getHeight();
+	intensity_map = new Intensity[n_pixels];
+	for (int i = 0; i < n_pixels; i++){
+		intensity_map[i] = other.getPixel(i);
+	}
+
+	//int throwError = 0, e = 1 / throwError;
+	//(DONE)TODO: Create a copy from the other object
 }
 
 IntensityImageStudent::IntensityImageStudent(const int width, const int height) : IntensityImage(width, height) {
+	intensity_map = new Intensity[width * height];
+	
 	int throwError = 0, e = 1 / throwError;
-	//TODO: Initialize pixel storage
+	//(DONE) TODO: Initialize pixel storage
 }
 
 IntensityImageStudent::~IntensityImageStudent() {
+
+	delete[] intensity_map;
+	
 	int throwError = 0, e = 1 / throwError;
-	//TODO: delete allocated objects
+	//(DONE)TODO: delete allocated objects
 }
 
 void IntensityImageStudent::set(const int width, const int height) {
 	IntensityImage::set(width, height);
-	int throwError = 0, e = 1 / throwError;
-	//TODO: resize or create a new pixel storage (Don't forget to delete the old storage)
+	delete[] intensity_map;
+	intensity_map = new Intensity[width * height];
+
+	//int throwError = 0, e = 1 / throwError;
+	//(DONE)TODO: resize or create a new pixel storage (Don't forget to delete the old storage)
 }
 
 void IntensityImageStudent::set(const IntensityImageStudent &other) {
 	IntensityImage::set(other.getWidth(), other.getHeight());
-	int throwError = 0, e = 1 / throwError;
-	//TODO: resize or create a new pixel storage and copy the object (Don't forget to delete the old storage)
+	delete[] intensity_map;
+	int n_pixels = other.getWidth() * other.getHeight();
+	intensity_map = new Intensity[n_pixels];
+
+	for (int i = 0; i < n_pixels; i++){
+		intensity_map[i] = other.getPixel(i);
+	}
+
+	
+	//int throwError = 0, e = 1 / throwError;
+	//(DONE)TODO: resize or create a new pixel storage and copy the object (Don't forget to delete the old storage)
 }
 
 void IntensityImageStudent::setPixel(int x, int y, Intensity pixel) {
-	int throwError = 0, e = 1 / throwError;
-	//TODO: no comment needed :)
+	
+	intensity_map[y * getWidth() + x] = pixel;
+	
+	//int throwError = 0, e = 1 / throwError;
+	//(DONE)TODO: no comment needed :)
 }
 
 void IntensityImageStudent::setPixel(int i, Intensity pixel) {
-	int throwError = 0, e = 1 / throwError;
+	
+	intensity_map[i] = pixel;
+	
+	//int throwError = 0, e = 1 / throwError;
 	/*
 	* TODO: set pixel i in "Row-Major Order"
 	*
@@ -63,13 +92,22 @@ void IntensityImageStudent::setPixel(int i, Intensity pixel) {
 }
 
 Intensity IntensityImageStudent::getPixel(int x, int y) const {
-	int throwError = 0, e = 1 / throwError;
-	//TODO: no comment needed :)
+	
+	return intensity_map[ y * getWidth() + x];
+
+	//int throwError = 0, e = 1 / throwError;
+	//(DONE)TODO: no comment needed :)
 	return 0;
 }
 
 Intensity IntensityImageStudent::getPixel(int i) const {
-	int throwError = 0, e = 1 / throwError;
-	//TODO: see setPixel(int i, RGB pixel)
+	
+	if (i <= getWidth()*getHeight()){
+		return intensity_map[i];
+	}
+
+	
+	//int throwError = 0, e = 1 / throwError;
+	//(DONE)TODO: see setPixel(int i, RGB pixel)
 	return 0;
 }
