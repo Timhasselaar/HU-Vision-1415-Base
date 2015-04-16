6e035044727aa6d6c8ac6c1e5a5054cce7795089
mcmarty026 <martijn.vanderstruijk@student.hu.nl>
2015-04-16 13:58:31 +0200

    stap2 week 1

--- source/ExternalDLL/ExternalDLL/StudentPreProcessing.cpp
+++ source/ExternalDLL/ExternalDLL/StudentPreProcessing.cpp
@@ -1,7 +1,23 @@
 #include "StudentPreProcessing.h"
-
+#include "IntensityImageStudent.h"
 
 IntensityImage * StudentPreProcessing::stepToIntensityImage(const RGBImage &image) const {
+	IntensityImage * i_image = new IntensityImageStudent();
+	int n_pixels = image.getHeight() * image.getWidth();
+
+	for (int i = 0; i <= n_pixels; i++){
+		RGB p = image.getPixel(i);
+		double r = (double)p.r * 0.21;
+		double g = (double)p.g * 0.72;
+		double b = (double)p.b * 0.07;
+		
+		unsigned char c =(int)( r + g + b);
+
+		i_image->setPixel(i,c);
+
+	}
+
+
 	return nullptr;
 }
 
