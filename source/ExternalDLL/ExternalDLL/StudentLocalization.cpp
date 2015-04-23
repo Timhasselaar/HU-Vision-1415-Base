7fe17b93e27994a1120c4c9eece0e82fc489f1bd
Timhasselaar <tim.hasselaar@student.hu.nl>
2015-04-23 12:45:54 +0200

    Base of eye localization

--- source/ExternalDLL/ExternalDLL/StudentLocalization.cpp
+++ source/ExternalDLL/ExternalDLL/StudentLocalization.cpp
@@ -24,15 +24,42 @@ bool StudentLocalization::stepFindExactEyes(const IntensityImage &image, Feature
 
 	//Create the eye features to return
 	Feature featureLeftEye = Feature(Feature::FEATURE_EYE_LEFT_RECT);
-	Feature featureRightEye = Feature(Feature::FEATURE_EYE_RIGHT_RECT);
+   Feature featureRightEye = Feature(Feature::FEATURE_EYE_RIGHT_RECT);
 
 	//Add the left eye rect
-	featureLeftEye.addPoint(Point2D<double>(10,10));
-	featureLeftEye.addPoint(Point2D<double>(20, 20));
+   int x_top_left_left_eye = 0;
+   int y_top_left_left_eye = 0;
+   int x_bottom_right_left_eye = 0;
+   int y_bottom_right_left_eye = 0;
+   featureLeftEye.addPoint(Point2D<double>(x_top_left_left_eye, y_top_left_left_eye));
+   featureLeftEye.addPoint(Point2D<double>(x_bottom_right_left_eye, y_bottom_right_left_eye));
 
 	//Add the right eye rect
-	featureRightEye.addPoint(Point2D<double>(30, 10));
-	featureRightEye.addPoint(Point2D<double>(40, 20));
+   int x_top_left_right_eye = 0;
+   int y_top_left_right_eye = 0;
+   int x_bottom_right_right_eye = 0;
+   int y_bottom_right_right_eye = 0;
+   featureRightEye.addPoint(Point2D<double>(x_top_left_right_eye, y_top_left_right_eye));
+   featureRightEye.addPoint(Point2D<double>(x_bottom_right_right_eye, y_bottom_right_right_eye));
+
+   //Draw rectangles on RGB_Map
+   for (int i = x_top_left_left_eye; i <= x_bottom_right_left_eye; i++){
+      debugImage->setPixel(i, y_top_left_left_eye, RGB(255, 255, 0));
+      debugImage->setPixel(i, y_bottom_right_left_eye, RGB(255, 255, 0));
+   }
+   for (int i = y_top_left_left_eye; i <= y_bottom_right_left_eye; i++){
+      debugImage->setPixel(x_top_left_left_eye, i, RGB(255, 255, 0));
+      debugImage->setPixel(x_bottom_right_left_eye, i, RGB(255, 255, 0));
+   }
+
+   for (int i = x_top_left_right_eye; i <= x_bottom_right_right_eye; i++){
+      debugImage->setPixel(i, y_top_left_right_eye, RGB(255, 255, 0));
+      debugImage->setPixel(i, y_bottom_right_right_eye, RGB(255, 255, 0));
+   }
+   for (int i = y_top_left_right_eye; i <= y_bottom_right_right_eye; i++){
+      debugImage->setPixel(x_top_left_right_eye, i, RGB(255, 255, 0));
+      debugImage->setPixel(x_bottom_right_right_eye, i, RGB(255, 255, 0));
+   }
 
 	//Put the eye features
 	features.putFeature(featureRightEye);
