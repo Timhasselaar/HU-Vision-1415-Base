d9d4ed42f5fa4a5ea899a68bc12e71556f0be6b8
Timhasselaar <tim.hasselaar@student.hu.nl>
2015-05-20 11:01:30 +0200

    Fixed edge detection

--- source/ExternalDLL/ExternalDLL/StudentPreProcessing.cpp
+++ source/ExternalDLL/ExternalDLL/StudentPreProcessing.cpp
@@ -29,40 +29,65 @@ IntensityImage * StudentPreProcessing::stepScaleImage(const IntensityImage &imag
 
 IntensityImage * StudentPreProcessing::stepEdgeDetection(const IntensityImage &image) const {
    IntensityImage * i_image = new IntensityImageStudent(image.getWidth(), image.getHeight());
-   int firstAndThirdThreeKernelLines[] = { 0, 0, 0, 1, 1, 1, 0, 0, 0 };
-   int secondThreeKernelLines[] = { 1, 1, 1, -4, -4, -4, 1, 1, 1 };
+   int max = 0; int min = 0;
+   int firstAndThirdThreeKernelLines[] = { 0, 0, 0, 1 ,1,1, 0, 0, 0 };
+   int secondThreeKernelLines[] = { 1,1,1,-4,-4,-4,1,1,1 };
    for (int image_x = 0; image_x < image.getWidth(); image_x++){
       for (int image_y = 0; image_y < image.getHeight(); image_y++){
          int total_intensity = 0;
-         for (int kernel_x = image_x - 4; kernel_x <= image_x + 4; kernel_x++){
-            for (int kernel_y = image_y - 4; kernel_y <= image_y + 4; kernel_y++){
-               int temp_kernel_x;
-               int temp_kernel_y;
+         for (int kernel_x = 0; kernel_x < 9; kernel_x++){
+            for (int kernel_y = 0; kernel_y < 9; kernel_y++){
+               int kernel_on_image_x = image_x + kernel_x - 4;
+               int kernel_on_image_y = image_y + kernel_y - 4;
 
                //First check if the x coordinate isn't outside of the image
-               if (kernel_x < 0){ temp_kernel_x = 0; }
-               else if (kernel_x >= image.getWidth()){ temp_kernel_x = image.getWidth() - 1; }
-               else{ temp_kernel_x = kernel_x; }
+               if (kernel_on_image_x < 0){ kernel_on_image_x = 0; }
+               else if (kernel_on_image_x >= image.getWidth()){ kernel_on_image_x = image.getWidth() - 1; }
+               else{ kernel_on_image_x = image_x + kernel_x - 4; }
 
 
                //Also check if the y coordinate isn't outside of the image
-               if (kernel_y < 0){ temp_kernel_y = 0; }
-               else if (kernel_y >= image.getHeight()){ temp_kernel_y = image.getHeight() - 1; }
-               else{ temp_kernel_y = kernel_y; }
+               if (kernel_on_image_y < 0){ kernel_on_image_y = 0; }
+               else if (kernel_on_image_y >= image.getHeight()){ kernel_on_image_y = image.getHeight() - 1; }
+               else{ kernel_on_image_y = image_y + kernel_y - 4; }
 
-               //Set what array is used for the kernel
-               if (kernel_y < image_y - 1 || kernel_y > image_y + 1){
-                  total_intensity += image.getPixel(temp_kernel_x, temp_kernel_y) * secondThreeKernelLines[kernel_x - image_x + 4];
+               if (kernel_y >= 3 && kernel_y <= 5){
+                  int ints = (int)image.getPixel(kernel_on_image_x, kernel_on_image_y);
+                  int nextints = ints * secondThreeKernelLines[kernel_x];
+                  total_intensity += nextints;
                }
-               else {
-                  total_intensity += image.getPixel(temp_kernel_x, temp_kernel_y) * firstAndThirdThreeKernelLines[kernel_x - image_x + 4];
+               else{
+                  total_intensity += (int)image.getPixel(kernel_on_image_x, kernel_on_image_y) * firstAndThirdThreeKernelLines[kernel_x];
                }
+               //std::cout << kernel_on_image_x << ", " << kernel_on_image_y << " = " << total_intensity << "\n";
             }
          }
-         int new_intensity = (-total_intensity / 36);
+         int new_intensity = 255 -(total_intensity/20 + 128);
+
+         /* Checking for the maximum and minimum values
+         if (new_intensity < min){
+            min = new_intensity;
+         }
+         if (new_intensity > max) {
+            max = new_intensity;
+         }
+         */
+
+         // quick thresholding, values obtained by testing
+         // TODO: Write test rapport on these values
+         if (new_intensity < 120){
+            new_intensity = 0;
+         }
+         else if (new_intensity > 200){
+            new_intensity = 0;
+         }
+         else {
+            new_intensity = 255;
+         }
          i_image->setPixel(image_x, image_y, new_intensity);
       }
    }
+   //std::cout << min << ", " << max;
    return i_image;
 }
 
